33dfc45852b0fe66750a7350bb672909c2b4fcbf
Corentin Dupont <corentin.dupont@gmail.com>
2019-09-27 20:58:59 +0200

    Update get_waziup.sh

--- setup/get_waziup.sh
+++ setup/get_waziup.sh
@@ -3,14 +3,6 @@
 
 WAZIUP_VERSION="V1.0-beta1"
 
-read -p "This script will download and install Wazigate. Continue (y/n)? "
-echo
-if [[ ! $REPLY =~ ^[Yy]$ ]]
-then
-    echo "Existing."
-    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
-fi
-
 echo "Downloading Wazigate..."
 sudo curl -fsSLO https://github.com/Waziup/waziup-gateway/archive/$WAZIUP_VERSION.tar.gz
 tar -xzvf $WAZIUP_VERSION.tar.gz
