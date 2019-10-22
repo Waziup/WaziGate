156bd6f563e6d5482bdda4f7ea771393d4949b8d
mojtaba-esk <mojtaba.esk@gmail.com>
2019-10-22 15:12:40 +0200

    updated

--- setup/get_waziup.sh
+++ setup/get_waziup.sh
@@ -8,7 +8,7 @@ WAZIUP_VERSION="V1.0-beta3"
 echo "Downloading Wazigate..."
 sudo curl -fsSLO https://github.com/Waziup/waziup-gateway/archive/$WAZIUP_VERSION.tar.gz
 tar -xzvf $WAZIUP_VERSION.tar.gz
-mv waziup-gateway-1.0-beta2 waziup-gateway
+mv waziup-gateway* waziup-gateway
 cd waziup-gateway
 chmod a+x setup/install.sh
 chmod a+x setup/uninstall.sh
