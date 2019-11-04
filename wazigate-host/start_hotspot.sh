e474491dc6d2cb693b1fd97439ae13fe27a7e969
mojtaba-esk <mojtaba.esk@gmail.com>
2019-11-04 14:00:52 +0100

    Wifi/Hotspot fixes

---
+++ wazigate-host/start_hotspot.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+sudo wpa_cli terminate -i wlan0
+sudo systemctl unmask hostapd.service
+sudo systemctl enable hostapd.service
+
+sudo systemctl stop dnsmasq
+sudo systemctl stop hostapd
+
+sudo service networking stop
+sleep 1
+#sudo cp setup/interfaces_ap /etc/network/interfaces
+#sudo rm /var/lib/dhcp/*
+sudo sh -c 'echo "interface wlan0" >> /etc/dhcpcd.conf'
+sudo sh -c 'echo "static ip_address=192.168.200.1/24" >> /etc/dhcpcd.conf'
+sudo sh -c 'echo "static routers=192.168.200.1" >> /etc/dhcpcd.conf'
+sudo sh -c 'echo "static domain_name_servers=192.168.200.1 8.8.8.8 fd51:42f8:caae:d92e::1" >> /etc/dhcpcd.conf'
+sudo service networking start
+
+#sudo service dnsmasq start; ';
+#sudo service hostapd start; ';
+#sudo service networking start; ';
+#sudo reboot; ';
+
+#	sudo systemctl restart networking
+
+sleep 1
+
+sudo sh -c "echo 1 > /proc/sys/net/ipv4/ip_forward"
+sudo sed -i 's/^#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
+sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+sudo iptables -A FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+sudo iptables -A FORWARD -i wlan0 -o eth0 -j ACCEPT
+
+sleep 1
+
+sudo systemctl unmask hostapd
+sudo systemctl enable hostapd
+sudo systemctl start hostapd
+sudo systemctl start dnsmasq
+
+sleep 1
+
+
+sudo service dnsmasq start
+sudo service hostapd start
+sudo service networking reload
+
+sudo service dhcpcd restart
+
+sleep 1
+
+#sudo systemctl restart networking
+
+exit 0;
---
+++ wazigate-host/start_wifi.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+sudo systemctl stop hostapd
+sudo systemctl stop dnsmasq
+
+sleep 1
+sudo systemctl disable hostapd.service
+
+sudo service networking stop
+sleep 1
+sudo sed -i '/^interface wlan0/d' /etc/dhcpcd.conf
+sudo sed -i '/^static ip_address=192.168.200.1\/24/d' /etc/dhcpcd.conf
+sudo sed -i '/^static routers=192.168.200.1/d' /etc/dhcpcd.conf
+sudo sed -i '/^static domain_name_servers=192.168.200.1.*/d' /etc/dhcpcd.conf
+#sudo mv /etc/network/interfaces /etc/network/interfaces_old;'
+#sudo rm /etc/network/interfaces
+sudo service networking start
+
+sleep 1
+
+sudo wpa_cli -i wlan0 reconfigure
+
+sleep 1
+sudo ip link set dev wlan0 down
+
+#sleep 1
+#sudo wpa_cli terminate
+
+sleep 2
+sudo ip link set dev wlan0 up
+
+
+sleep 1
+sudo systemctl restart dhcpcd.service
+
+sudo wpa_supplicant -B -i wlan0 -c /etc/wpa_supplicant/wpa_supplicant.conf
+
+sleep 1
+
+sudo systemctl restart networking
+
+exit 0;
