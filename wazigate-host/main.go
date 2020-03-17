fc90df9113a21d61d155d36de7a5cc19b8ff2fe6
mojtaba-esk <mojtaba.esk@gmail.com>
2020-03-17 12:01:12 +0100

    use unix socket

--- wazigate-host/main.go
+++ wazigate-host/main.go
@@ -1,35 +1,40 @@
-/* 
-	@author: mojtaba.eskandari@waziup.org Nov 25th 2019
-	@A deamon to execute commands on host
-*/
+/*
+*	@author: mojtaba.eskandari@waziup.org Nov 25th 2019
+*	@A deamon to execute commands on host
+ */
 package main
 
 import (
 	// "fmt"
-	"os"
-	"log"
 	"flag"
+	"log"
+	"net"
+	"os"
+
 	// "time"
 	"net/http"
 	// "encoding/json"
 	"strings"
 	// "strconv"
 
+	"io/ioutil"
 	"os/exec"
 	"path/filepath"
-	"io/ioutil"
 
-	routing "github.com/julienschmidt/httprouter"	
+	routing "github.com/julienschmidt/httprouter"
 )
 
 /*-------------------------*/
+
+// sockAddr represents the unix socket for this service
+const sockAddr = "/var/run/wazigate-host.sock"
 
 var router = routing.New()
 
 func init() {
 
-	router.GET( "/", homeLink)
-	router.POST( "/cmd", execCommand)
+	router.GET("/", homeLink)
+	router.POST("/cmd", execCommand)
 }
 
 /*-------------------------*/
@@ -38,25 +43,29 @@ var execPath = ""
 
 func main() {
 
-	log.Printf( "Initializing...")
+	log.Printf("Initializing...")
 
-	dir, err := filepath.Abs( filepath.Dir( os.Args[0]))
+	if err := os.RemoveAll(sockAddr); err != nil {
+		log.Fatal(err)
+	}
+
+	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
 	if err != nil {
-		log.Fatal( err)
+		log.Fatal(err)
 	}
 	execPath = dir
 
 	// log.Printf( dir)
 
-	host := flag.String( "s", "", "Server address")
-	port := flag.String( "p", "5200", "Port number")
-	debugMode := flag.String( "d", "0", "Debug Mode")
+	// host := flag.String("s", "", "Server address")
+	// port := flag.String("p", "5200", "Port number")
+	debugMode := flag.String("d", "0", "Debug Mode")
 
 	flag.Parse()
 
 	if *debugMode != "0" {
 		// If debug mode
-		f, err := os.OpenFile( execPath +"/host.logs", os.O_RDWR | os.O_CREATE | os.O_APPEND, 0666)
+		f, err := os.OpenFile(execPath+"/host.logs", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 		if err != nil {
 			log.Fatalf("[Err   ]: Error opening file: %v", err)
 		}
@@ -65,48 +74,59 @@ func main() {
 		log.SetOutput(f)
 	}
 
-	ListenAndServeHTTP( *host +":"+ *port)
+	// ListenAndServeHTTP(*host + ":" + *port)
+
+	server := http.Server{
+		Handler: router,
+	}
+
+	l, e := net.Listen("unix", sockAddr)
+	if e != nil {
+		log.Fatal("listen error:", e)
+	}
+	log.Printf("Serving... on socket: [%v]", sockAddr)
+	server.Serve(l)
 }
 
 /*-------------------------*/
 
-func ListenAndServeHTTP( addr string) {
+// func ListenAndServeHTTP(addr string) {
 
-	log.Printf( "[Info  ] Serving on %s", addr)
-	log.Fatal( http.ListenAndServe( addr, router))
+// 	log.Printf("[Info  ] Serving on %s", addr)
+// 	log.Fatal(http.ListenAndServe(addr, router))
+// }
+
+/*-------------------------*/
+
+func homeLink(resp http.ResponseWriter, req *http.Request, params routing.Params) {
+	resp.Write([]byte("Salam, It works!"))
 }
 
 /*-------------------------*/
 
-func homeLink( resp http.ResponseWriter, req *http.Request, params routing.Params) {
-	resp.Write( []byte( "Salam, It works!"))
-}
+func execCommand(resp http.ResponseWriter, req *http.Request, params routing.Params) {
 
-/*-------------------------*/
-
-func execCommand( resp http.ResponseWriter, req *http.Request, params routing.Params) {
-	
 	// cmd := string( req.Body)
-	cmd, err := ioutil.ReadAll( req.Body)
-	if( err != nil) {
-		log.Printf( "[Err   ] executing [ %s ] command. \n\tError: [ %s ]", cmd, err.Error())
-		http.Error( resp, err.Error(), http.StatusBadRequest)
+	cmd, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		log.Printf("[Err   ] executing [ %s ] command. \n\tError: [ %s ]", cmd, err.Error())
+		http.Error(resp, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	log.Printf( "[Info  ] executing [ %s ] ", cmd)
+	log.Printf("[Info  ] executing [ %s ] ", cmd)
 
-	exe := exec.Command( "sh", "-c", string( cmd))
+	exe := exec.Command("sh", "-c", string(cmd))
 	exe.Dir = execPath
-    stdout, err := exe.Output()
+	stdout, err := exe.Output()
 
-    if( err != nil) {
-		log.Printf( "[Err   ] executing [ %s ] command. \n\tError: [ %s ]", cmd, err.Error())
-		http.Error( resp, err.Error(), http.StatusBadRequest)
+	if err != nil {
+		log.Printf("[Err   ] executing [ %s ] command. \n\tError: [ %s ]", cmd, err.Error())
+		http.Error(resp, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	out := strings.Trim( string( stdout), " \n\t\r")
-	
-	resp.Write( []byte( out))
+	out := strings.Trim(string(stdout), " \n\t\r")
+
+	resp.Write([]byte(out))
 }
