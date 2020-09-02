0a7178285d7f116720298bbe9ee650eb9628a5d0
mojtaba-esk <mojtaba.esk@gmail.com>
2020-09-02 15:50:22 +0200

    some fixes

--- stop.sh
+++ stop.sh
@@ -3,7 +3,8 @@
 
 SCRIPT_PATH=$(dirname $(realpath $0))
 
-sudo docker-compose stop
+#sudo docker-compose stop
+sudo docker stop $(sudo docker ps -q)
 
 #Kill wazigate-host
 ps ax | grep "wazigate-host" | awk '{print $1}' | sudo xargs kill
