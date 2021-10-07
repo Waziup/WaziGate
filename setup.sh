95d99e485da5f080fb53a85bc79127df44865094
Johann Forster <25847959+j-forster@users.noreply.github.com>
2021-10-07 11:13:48 +0200

    Prepare for pi-gen

--- setup.sh
+++ setup.sh
@@ -1,24 +1,16 @@
 #!/bin/bash -e
 
-# Set the SSID for Access Point using the Raspberry Pi MAC address
-if [ ! -f .default_ap_done ] ; then
-
-	MAC="Access_Point"
-
-	if [ -f  /sys/class/net/eth0/address ] ; then
-		MAC=$(cat /sys/class/net/eth0/address)
-	else
-		if [ -f  /sys/class/net/wlan0/address ] ; then
-			MAC=$(cat /sys/class/net/wlan0/address)
-		fi;
+if [ -f  /sys/class/net/eth0/address ] ; then
+	WAZIGATE_ID=$(cat /sys/class/net/eth0/address)
+else
+	if [ -f  /sys/class/net/wlan0/address ] ; then
+		WAZIGATE_ID=$(cat /sys/class/net/wlan0/address)
 	fi;
-
-	MAC=${MAC//:}
-	SSID="WAZIGATE_${MAC^^}"
-	sudo sed -i "s/^ssid.*/ssid=$SSID/g" /etc/hostapd/hostapd.conf
-
-	touch .default_ap_done	
 fi;
+WAZIGATE_ID=${WAZIGATE_ID//:}
+
+SSID="WAZIGATE_${WAZIGATE_ID^^}"
+sed -i "s/^ssid.*/ssid=$SSID/g" /etc/hostapd/hostapd.conf
 
 # Create the docker network and containers if they do not exist
 if ! docker network inspect wazigate > /dev/null; then
@@ -29,13 +21,27 @@ fi
 if ! docker image inspect wazigate-mongo --format {{.Id}} > /dev/null; then
   echo "Creating container 'wazigate-mongo' (MongoDB) ..."
   # docker image save webhippie/mongodb -o wazigate-mongo.tar
-  docker image load -i wazigate-mongo.tar
-  docker-compose run -d wazigate-mongo
+  WAZIGATE_MONGO=$(docker image load -i wazigate-mongo.tar | sed -n 's/.*sha256://p')
+  docker tag $WAZIGATE_MONGO webhippie/mongodb
+  docker run -d --restart=always --network=wazigate --name wazigate-mongo \
+	-p "27017:27017" \
+	-v "$PWD/wazigate-mongo/data:/var/lib/mongodb" \
+	-v "$PWD/wazigate-mongo/backup:/var/lib/backup" \
+	-v "$PWD/wazigate-mongo/bin:/var/lib/bin" \
+	--health-cmd="echo 'db.stats().ok' | mongo localhost:27017/local --quiet" \
+	--health-interval=10s \
+	--entrypoint="sh" \
+	webhippie/mongodb \
+	/var/lib/bin/entrypoint.sh
 fi
 
 if ! docker image inspect wazigate-edge --format {{.Id}} > /dev/null; then
   echo "Creating container 'wazigate-edge' (Wazigate Edge) ..."
   # docker image save waziup/wazigate-edge -o wazigate-edge.tar
-  docker image load -i wazigate-edge.tar
-  docker-compose run -d wazigate-edge
+  WAZIGATE_EDGE=$(docker image load -i wazigate-edge.tar | sed -n 's/.*sha256://p')
+  docker tag $WAZIGATE_EDGE waziup/wazigate-edge
+  docker run -d --restart=always --network=wazigate --name wazigate-edge \
+  	-e "WAZIGATE_ID=$WAZIGATE_ID" \
+	-p "80:80" -p "1883:1883" \
+	waziup/wazigate-edge
 fi
--- wazigate-mongo/bin/entrypoint.sh
+++ wazigate-mongo/bin/entrypoint.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 # This file initiates the mongo db
 
 /etc/init.d/mongodb stop
 rm -f /var/lib/mongodb/mongod.lock
 mongod --repair --dbpath=/var/lib/mongodb/
-mongod --dbpath=/var/lib/mongodb/
+exec mongod --dbpath=/var/lib/mongodb/
