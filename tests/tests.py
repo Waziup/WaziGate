d03ed72634e4202a7d649cfc4b64f038ecd03396
Corentin Dupont <corentin.dupont@gmail.com>
2022-02-17 16:40:18 +0100

    logging

--- tests/tests.py
+++ tests/tests.py
@@ -10,8 +10,14 @@ import logging
 import time
 import os
 import sys
+import logging
 from xmlrunner import XMLTestRunner
 
+logging.basicConfig()
+logging.getLogger().setLevel(logging.DEBUG)
+requests_log = logging.getLogger("requests.packages.urllib3")
+requests_log.setLevel(logging.DEBUG)
+requests_log.propagate = True
 
 ## Variable declaration
 
