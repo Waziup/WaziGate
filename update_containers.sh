e5359a0a95290b67e4da4de92026c47ae81bab5f
Corentin Dupont <corentin.dupont@gmail.com>
2022-03-12 19:55:43 +0100

    jenkins

--- update_containers.sh
+++ update_containers.sh
@@ -8,9 +8,10 @@ sudo systemctl restart docker
 docker-compose down
 sleep 5
 
+# Load new image
 if [ -f wazigate_images.tar ]; then
   docker load -i wazigate_images.tar
-  rm -f wazigate_images.tar
+  sudo rm -f wazigate_images.tar
 fi
 
 # Restart
